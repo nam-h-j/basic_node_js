6f4e47da0efb833378758b0e498fda3828fb580d
nam_hyunjae <>
2022-03-30 20:07:06 +0900

    server setting

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hey");
+var http = require("http");
+var fs = require("fs");
+var port = 3000;
+var app = http.createServer(function (request, response) {
+  var url = request.url;
+  if (request.url == "/") {
+    url = "/index.html";
+  }
+  if (request.url == "/favicon.ico") {
+    return response.writeHead(404);
+  }
+  response.writeHead(200);
+  response.end(fs.readFileSync(__dirname + url));
+});
+app.listen(port);
+console.log("server start....");
+console.log(`run in : //localhost:${3000}`);
